a98c0bb296fb2c4c6b2e0bcec3dd59ae887ba80b
Nick Mitchell <nickm@us.ibm.com>
2025-04-28 09:09:13 -0400

    feat: use clap to parse --model and --temperature command line args
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- spl_cli/src/main.rs
+++ spl_cli/src/main.rs
@@ -1,31 +1,43 @@
 use spl_ast::spl;
 use spl_run::{result::SplError, run};
 
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Model
+    #[arg(short, long, default_value = "ollama/granite3.2:2b")]
+    model: String,
+
+    /// Temperature
+    #[arg(short, long, default_value_t = 0.0)]
+    temperature: f32,
+}
 #[tokio::main]
 async fn main() -> Result<(), SplError> {
+    let args = Args::parse();
+    let model = args.model;
+    let temp = args.temperature;
+
     let program = spl!(
-        let (//(model "ollama/granite3.2:2b"))
-            (model "openai/ibm-granite/granite-3.3-8b-instruct"))
-            (g model
-             (cross "Ask the model to select the best option from the candidates"
-              (let
-               ((max_tokens (ask "Max length of email?" 100))
-                (temp (ask "Temperature?" 0.5))
-                (prompt (format "write an introductory email for a job application, limited to at most {max_tokens} characters. use your imagination, go wild")))
+        g model
+         (cross "Ask the model to select the best option from the candidates"
+          (let
+           ((max_tokens (ask "Max length of email?" 100))
+            (prompt (format "write an introductory email for a job application, limited to at most {max_tokens} characters. use your imagination, go wild")))
 
-               (plus "Generate candidate emails in parallel"
-                (g model prompt max_tokens temp)
-                (g model prompt max_tokens temp)
-                (g model prompt max_tokens temp)
-                (g model prompt max_tokens temp)
-                (g model prompt max_tokens temp)
-               )
-              )
+           (plus "Generate candidate emails in parallel"
+            (g model prompt max_tokens temp)
+            (g model prompt max_tokens temp)
+            (g model prompt max_tokens temp)
+            (g model prompt max_tokens temp)
+            (g model prompt max_tokens temp)
+           )
+          )
 
-              "Compute an evaluation score that ranks each of the given candidate introductory emails, respond with a list such as [3,1,2,4] which ranks the emails from best to worst and uses their input index, and then print the best one and explain your thinking."
+          "Compute an evaluation score that ranks each of the given candidate introductory emails, respond with a list such as [3,1,2,4] which ranks the emails from best to worst and uses their input index, and then print the best one and explain your thinking."
 
-             )
-            )
+         )
     );
 
     let res = run(&program, None).await?;
