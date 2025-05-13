0e32040bb3f3e07dc31eef363604ba9607c6d5d4
Nick Mitchell <nickm@us.ibm.com>
2025-05-13 09:32:03 -0400

    refactor: in web ui, pull demo queries into separate files
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- spnl_web/src/DemoSelect.tsx
+++ spnl_web/src/DemoSelect.tsx
@@ -9,53 +9,22 @@ import {
   type MenuToggleElement,
 } from "@patternfly/react-core"
 
+import email from "./demos/email.lisp?raw"
+import email2 from "./demos/email2.lisp?raw"
+
 const demos = [
   {
     label: "Email Judge/Generator",
     description:
       "This demo is the simplest query, but does not generate great output",
-    value: `(g "ollama/granite3.2:2b"
-   (cross
-    (system "You compute an evaluation score from 0 to 100 that ranks given candidate introductory emails. Better emails are ones that mention specifics, such as names of people and companies. You present a list of the top 3 ordered by their rank showing the score and full content of each.")
-
-    (print "Generate 4 candidate emails in parallel")
-    (plus
-     (repeat 4
-             (g "ollama/granite3.2:2b"
-                (user "write an introductory email for a job application, limited to at most 100 characters.")
-                100 0.3)))
-
-    (print "Done generating candidate emails. Now asking the model to select the best option from the candidates")
-    (user "My name is Shiloh. I am a data scientist with 10 years of experience and need an introductory email to apply for a position at IBM in their research department"))
-0 0.0)`,
+    value: email,
   },
 
   {
     label: "Improved Email Judge/Generator",
     description:
       "This demo generates better output, at the expense of a more complicated query",
-    value: `(g "ollama/granite3.2:2b"
-   (cross
-    (system "You compute an evaluation score from 0 to 100 that ranks given candidate introductory emails. Better emails are ones that mention specifics, such as names of people and companies. You present a list of the top 3 ordered by their rank showing the score and full content of each.")
-
-    (print "Generate 4 candidate emails in parallel")
-    (plus
-     (repeat 4
-             (g "ollama/granite3.2:2b"
-                (cross
-                 (system "You compute an evaluation score from 0 to 100 that ranks given candidate introductory emails. Better emails are ones that mention specifics, such as names of people and companies. You present a list of the top 3 ordered by their rank showing the score and full content of each.")
-                 (user "write an introductory email for a job application, limited to at most 100 characters.")
-
-                 (user "My name is Shiloh. I am a data scientist with 10 years of experience and need an introductory email to apply for a position at IBM in their research department")
-                 )
-
-                100 0.2
-                )
-             )
-     )
-    )
-   0 0.0
-   )`,
+    value: email2,
   },
 ]
 
@@ -74,7 +43,7 @@ export default function DemoSelect() {
     value: string | number | undefined,
   ) => {
     // eslint-disable-next-line no-console
-    console.log("selected", value)
+    // console.log("selected", value)
 
     setSelected(value as string)
     setIsOpen(false)
