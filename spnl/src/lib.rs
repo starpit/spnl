a6859ca5dd346a5aaef10782b03cc5d5f2fe12c7
Nick Mitchell <nickm@us.ibm.com>
2025-06-15 17:38:59 -0400

    refactor: pull lisp macros into separate file and feature flag
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- spnl/src/lib.rs
+++ spnl/src/lib.rs
@@ -1,197 +1,12 @@
 pub mod run;
 
+#[cfg(feature = "lisp")]
+mod lisp;
+
 #[cfg(feature = "python_bindings")]
 mod python_bindings;
 #[cfg(feature = "python_bindings")]
 pub use python_bindings::spnl;
-
-// Inspiration: https://github.com/JunSuzukiJapan/macro-lisp
-#[macro_export]
-macro_rules! spnl {
-    // Core: Generate text given $input using $model
-    (g $model:tt $input:tt) => ($crate::spnl!(g $model $input 0.0 0));
-
-    // Core: Generate text given $input using $model with temperature $temp
-    (g $model:tt $input:tt $temp:tt) => ($crate::spnl!(g $model $input $temp 0));
-
-    // Core: Generate text given $input using $model with temperature $temp and $max_tokens
-    (g $model:tt $input:tt $temp:tt $max_tokens:tt) => ($crate::spnl!(g $model $input $temp $max_tokens false));
-
-    (g $model:tt $input:tt $temp:tt $max_tokens:tt $accumulate:tt) => (
-        $crate::Unit::Generate((
-            $crate::spnl_arg!($model).to_string(),
-            Box::new($crate::spnl_arg!($input).into()),
-            $crate::spnl_arg!($max_tokens), $crate::spnl_arg!($temp), $crate::spnl_arg!($accumulate),
-        ))
-    );
-
-    // Core: Generate with accumulation
-    (gx $model:tt $input:tt) => ($crate::spnl!(g $model $input 0.0 0 true));
-
-    // Core: Generate with accumulation with temperature $temp
-    (gx $model:tt $input:tt $temp:tt) => ($crate::spnl!(g $model $input $temp 0 true));
-
-    // Core: Generate with accumulation with temperature $temp and $max_tokens
-    (gx $model:tt $input:tt $temp:tt $max_tokens:tt) => ($create::spnl!(g $model $input $temp $max_tokens true));
-
-    // Core: Dependent/needs-attention
-    (cross $( $e:tt )+) => ( $crate::Unit::Cross(vec![$( $crate::spnl_arg!( $e ).into() ),+]) );
-
-    // Core: Independent/no-attention with one or more inputs provided directly as a vector
-    (plus $e:tt) => ( $crate::Unit::Plus($crate::spnl_arg!( $e )) );
-
-    // Core: Independent/no-attention with multiple inputs provided inline
-    (plus $( $e:tt )+) => ( $crate::Unit::Plus(vec![$( $crate::spnl_arg!( $e ).into() ),+]) );
-
-    // Core: A user message
-    (user $e:tt) => ($crate::Unit::User(($crate::spnl_arg!($e).clone().into(),)));
-
-    // Core: A system message
-    (system $e:tt) => ($crate::Unit::System(($crate::spnl_arg!($e).into(),)));
-
-    // Data: incorporate a file at compile time
-    (file $f:tt) => (include_str!($crate::spnl_arg!($f)));
-
-    // Data: incorporate a file at compile time, preserving file name
-    (filen $f:tt) => (($crate::spnl_arg!($f).to_string(), include_str!($crate::spnl_arg!($f)).to_string()));
-
-    // Data: incorporate a file at run time
-    (fetch $f:tt) => (match $crate::spnl!(fetchn $f).1 { $crate::Document::Text(a) => a,  $crate::Document::Binary(b) => String::from_utf8(b).expect("string") });
-
-    // Data: incorporate a file at run time, preserving file name
-    (fetchn $f:tt) => {{
-        let filename = ::std::path::Path::new(file!()).parent().expect("macro to have parent directory").join($crate::spnl_arg!($f));
-        (filename.clone().into_os_string().into_string().expect("filename"), $crate::Document::Text(::std::fs::read_to_string(filename).expect("error reading file")))
-    }};
-
-    // Data: incorporate a binary file at run time, preserving file name
-    (fetchb $f:tt) => {{
-        let filename = ::std::path::Path::new(file!()).parent().expect("macro to have parent directory").join($crate::spnl_arg!($f));
-        (filename.clone().into_os_string().into_string().expect("filename"), $crate::Document::Binary(::std::fs::read(filename).expect("error reading file")))
-    }};
-
-    // Data: peel off the first $n elements of the given serialized
-    // json vector of strings (TODO: split this into multiple macros)
-    (take $n:tt $s:tt) => (
-        #[cfg(feature = "rag")]
-        serde_json::from_str::<Vec<String>>($crate::spnl_arg!($s))?
-            .into_iter()
-            .take($crate::spnl_arg!($n).try_into().expect("usize"))
-            .collect::<Vec<_>>()
-    );
-
-    // Data: prefix every string in $arr with $p
-    (prefix $p:tt $arr:tt) => (
-        $crate::spnl_arg!($arr)
-            .into_iter()
-            .enumerate()
-            .map(|(idx, s)| ((1 + idx), s)) // (idx % $crate::spnl_arg!($chunk_size)), s))
-            .map(|(idx, s)| $crate::spnl!(user (format "{}{idx}: {:?}" $p s)))
-            .collect::<Vec<_>>()
-    );
-
-    // Data: break up the array $arr into chunks of maximum size
-    // $chunk_size characters and send each chunk to the given
-    // (lambda) $f.
-    (chunk $chunk_size:tt $arr:tt $f:tt) => (
-        $crate::spnl_arg!($arr)
-            .chunks($crate::spnl_arg!($chunk_size))
-            .map(|chunk| chunk.to_vec())
-            .map($crate::spnl_arg!($f))
-            .collect::<Vec<_>>()
-    );
-
-    // Data: incorporate one or more documents
-    (with $embedding_model:tt $input:tt $( $doc:tt )+) => (
-        $crate::spnl!(
-            cross
-                (plus $( (__spnl_retrieve $embedding_model $input $doc) )+)
-                (user "Please answer this question:")
-                $input
-        )
-    );
-
-    // Internal
-    (__spnl_retrieve $embedding_model:tt $input:tt $doc:tt) => (
-        vec![$crate::Unit::Retrieve(
-            ($crate::spnl_arg!($embedding_model),
-             Box::new($crate::spnl_arg!($input)),
-             $crate::spnl_arg!( $doc ).into()) )]
-    );
-
-    // Sugar: this unfolds to a `(g $model (cross $body))` but with
-    // special user and system messages geared at extracting,
-    // simplifying, and summarizing the thought process of the output
-    // of prior (g) calls.
-    (extract $model:tt $n:tt $body:tt) => {{
-        let n = $crate::spnl_arg!($n);
-        $crate::spnl!(
-            g $model (cross
-                      (system "Your are an AI that combines prior outputs from other AIs, preferring no markdown or other exposition.")
-                      $body
-                      (user (format "Extract and simplify these {} final answers" n))))
-    }};
-
-    // Sugar: this unfolds to a `(g $model (cross $body))` but with
-    // special user and system messages geared at combining the
-    // output of prior (g) calls.
-    (combine $model:tt $body:tt) => (
-        $crate::spnl!(
-            g $model (cross
-                      (system "Your are an AI that combines prior outputs from other AIs, preferring no markdown or other exposition.")
-                      $body
-                      (user "Combine and flatten these into one JSON array, preserving order")))
-    );
-
-    // Sugar: this unfolds to repeating the given expression $e $n times.
-    (repeat $n:tt $e:tt) => (spnl!(repeat i $n $e));
-
-    // Sugar: this unfolds to repeating the given expression $e $n
-    // times and makes available an index variable $i ranging from 0
-    // to $n-1.
-    (repeat $i:ident $n:tt $e:tt) => (spnl!(repeat $i 0 $n $e));
-
-    // Sugar: this unfolds to repeating the given expression $e $n
-    // times and makes available an index variable $i ranging from
-    // $start to $n-$start-1.
-    (repeat $i:ident $start:tt $n:tt $e:tt) => {{
-        let mut args: Vec<$crate::Unit> = vec![];
-        let start = $crate::spnl_arg!($start);
-        let end = $crate::spnl_arg!($n) + start;
-        for $i in start..end {
-            args.push($crate::spnl_arg!($e).clone());
-        }
-        args
-    }};
-
-    // Utility: Defines an n-ary function that accepts the given $name'd arguments
-    (lambda ( $( $name:ident )* )
-     $( ( $($e:tt)* ))*
-    ) => (| $($name: Vec<Unit>),* |{ $( $crate::spnl!( $($e)* ) );* });
-
-    // Utility: the length of $list
-    (length $list:tt) => ($crate::spnl_arg!($list).len());
-
-    // Utility: read as string from stdin
-    (ask $message:tt) => ( $crate::Unit::Ask(($crate::spnl_arg!($message).into(),)) );
-
-    // Utility: print a helpful message to the console
-    (print $message:tt) => ( $crate::Unit::Print(($crate::spnl_arg!($message).into(),)) );
-
-    // Utility:
-    (format $fmt:tt $( $e:tt )*) => ( &format!($fmt, $($crate::spnl_arg!($e)),* ) );
-
-    // execute rust
-    //(rust $( $st:stmt )* ) => ( $($st);* );
-    // other
-    //($e:expr) => ($e.into());
-}
-
-#[macro_export]
-macro_rules! spnl_arg {
-    ( ( $($e:tt)* ) ) => ( $crate::spnl!( $($e)* ) );
-    ($e:expr) => ($e);
-}
 
 #[derive(Debug, Clone, PartialEq, serde::Deserialize, serde::Serialize)]
 pub enum Document {
@@ -347,105 +162,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn macro_user() {
-        let result = spnl!(user "hello");
-        assert_eq!(result, Unit::User(("hello".to_string(),)));
-    }
-    #[test]
-    fn macro_system() {
-        let result = spnl!(system "hello");
-        assert_eq!(result, Unit::System(("hello".to_string(),)));
-    }
-    #[test]
-    fn macro_ask() {
-        let result = spnl!(ask "hello");
-        assert_eq!(result, Unit::Ask(("hello".to_string(),)));
-    }
-    #[test]
-    fn macro_plus_1() {
-        let result = spnl!(plus (user "hello") (user "world"));
-        assert_eq!(
-            result,
-            Unit::Plus(vec![
-                Unit::User(("hello".to_string(),)),
-                Unit::User(("world".to_string(),))
-            ])
-        );
-    }
-    #[test]
-    fn macro_plus_2() {
-        let result = spnl!(plus (user "hello") (system "world"));
-        assert_eq!(
-            result,
-            Unit::Plus(vec![
-                Unit::User(("hello".to_string(),)),
-                Unit::System(("world".to_string(),))
-            ])
-        );
-    }
-    #[test]
-    fn macro_cross_1() {
-        let result = spnl!(cross (user "hello"));
-        assert_eq!(
-            result,
-            Unit::Cross(vec![Unit::User(("hello".to_string(),))])
-        );
-    }
-    #[test]
-    fn macro_cross_3() {
-        let result =
-            spnl!(cross (user "hello") (system "world") (plus (user "sloop") (user "boop")));
-        assert_eq!(
-            result,
-            Unit::Cross(vec![
-                Unit::User(("hello".to_string(),)),
-                Unit::System(("world".to_string(),)),
-                Unit::Plus(vec![
-                    Unit::User(("sloop".to_string(),)),
-                    Unit::User(("boop".to_string(),))
-                ])
-            ])
-        );
-    }
-    #[test]
-    fn macro_gen() {
-        let result = spnl!(g "ollama/granite3.2:2b" (user "hello") 0.0 0);
-        assert_eq!(
-            result,
-            Unit::Generate((
-                "ollama/granite3.2:2b".to_string(),
-                Box::new(Unit::User(("hello".to_string(),))),
-                0,
-                0.0,
-                false
-            ))
-        );
-    }
-
-    #[test]
     fn serde_user() -> serde_lexpr::Result<()> {
         let result = from_str("(user \"hello\")")?;
         assert_eq!(result, Unit::User(("hello".to_string(),)));
         Ok(())
     }
+
     #[test]
     fn serde_system() -> serde_lexpr::Result<()> {
         let result = from_str("(system \"hello\")")?;
         assert_eq!(result, Unit::System(("hello".to_string(),)));
         Ok(())
     }
+
     #[test]
     fn serde_ask() -> serde_lexpr::Result<()> {
         let result = from_str("(ask \"hello\")")?;
         assert_eq!(result, Unit::Ask(("hello".to_string(),)));
         Ok(())
     }
+
     #[test]
     fn serde_plus_1() -> serde_lexpr::Result<()> {
         let result = from_str("(plus (user \"hello\"))")?;
         assert_eq!(result, Unit::Plus(vec![Unit::User(("hello".to_string(),))]));
         Ok(())
     }
+
     #[test]
     fn serde_plus_2() -> serde_lexpr::Result<()> {
         let result = from_str("(plus (user \"hello\") (system \"world\"))")?;
@@ -458,6 +201,7 @@ mod tests {
         );
         Ok(())
     }
+
     #[test]
     fn serde_cross_1() -> serde_lexpr::Result<()> {
         let result = from_str("(cross (user \"hello\"))")?;
@@ -467,6 +211,7 @@ mod tests {
         );
         Ok(())
     }
+
     #[test]
     fn serde_cross_3() -> serde_lexpr::Result<()> {
         let result =
@@ -481,6 +226,7 @@ mod tests {
         );
         Ok(())
     }
+
     #[test]
     fn serde_gen() -> serde_lexpr::Result<()> {
         let result = from_str("(g \"ollama/granite3.2:2b\" (user \"hello\") 0 0.0 #f)")?;
@@ -497,45 +243,3 @@ mod tests {
         Ok(())
     }
 }
-
-// math
-/*(+ $x:tt $y:tt) => ($crate::spnl_arg!($x) + $crate::spnl_arg!($y));*/
-//(- $x:tt $y:tt) => ($crate::spnl_arg!($x) - $crate::spnl_arg!($y));
-/*(* $x:tt $y:tt) => ($crate::spnl_arg!($x) * $crate::spnl_arg!($y));
-(/ $x:tt $y:tt) => ($crate::spnl_arg!($x) / $crate::spnl_arg!($y));
-(% $x:tt $y:tt) => ($crate::spnl_arg!($x) % $crate::spnl_arg!($y));*/
-
-// bool
-//(false) => ($crate::Unit::Bool(false));
-//(true) => ($crate::Unit::Bool(true));
-//(self $(. $e:tt)* ) => (self $(. $e)* );
-
-// let
-/* (let ( $( ($var:ident $e:tt) )* )
-    $( ( $($e2:tt)* ) )*
-) => ({
-    $(let mut $var = $crate::spnl_arg!($e);)*
-    $( $crate::spnl!( $($e2)* ) );*
-}); */
-
-// dotimes
-/*(dotimes ($var:ident $count:tt) $( ( $($e:tt)* ) )* ) => (
-    for $var in 0..$crate::spnl_arg!($count) {
-        $( $crate::spnl!( $($e)* ) );*
-    }
-);*/
-
-// if
-/*(if ( $($cond:tt)* ) $e1:tt $e2:tt) => (if $crate::spnl!($($cond)*) { $crate::spnl!($e1) }else{ $crate::spnl!($e2) });
-(if ( $($cond:tt)* ) $e:tt) => (if $crate::spnl!($($cond)*) { $crate::spnl!($e) });
-(if $cond:tt $e1:tt $e2:tt) => (if $cond { $crate::spnl!($e1) }else{ $crate::spnl!($e2) });
-(if $cond:tt $e:tt) => (if $cond { $crate::spnl!($e) });*/
-
-// compare
-/*(eq $x:tt $y:tt) => ($crate::spnl_arg!($x) == $crate::spnl_arg!($y));
-(== $x:tt $y:tt) => ($crate::spnl_arg!($x) == $crate::spnl_arg!($y));
-(!= $x:tt $y:tt) => ($crate::spnl_arg!($x) != $crate::spnl_arg!($y));
-(< $x:tt $y:tt) => ($crate::spnl_arg!($x) < $crate::spnl_arg!($y));
-(> $x:tt $y:tt) => ($crate::spnl_arg!($x) > $crate::spnl_arg!($y));
-(<= $x:tt $y:tt) => ($crate::spnl_arg!($x) <= $crate::spnl_arg!($y));
-(>= $x:tt $y:tt) => ($crate::spnl_arg!($x) >= $crate::spnl_arg!($y));*/
