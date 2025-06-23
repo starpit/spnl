8db69b142ce49b40493595a2882439811ecb1060
Nick Mitchell <nickm@us.ibm.com>
2025-06-23 10:11:01 -0400

    fix: another clippy fix
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- spnl/src/run/extract.rs
+++ spnl/src/run/extract.rs
@@ -1,33 +1,27 @@
 use crate::{Generate, Query};
 
-/// Extract models referenced by the program
-pub fn extract_models(program: &Query) -> Vec<String> {
-    extract_values(program, "model")
-}
-
-/// Take a list of Yaml fragments and produce a vector of the string-valued entries of the given field
-fn extract_values(program: &Query, field: &str) -> Vec<String> {
+/// Extract models referenced by the query
+pub fn extract_models(query: &Query) -> Vec<String> {
     let mut values = vec![];
-    extract_values_iter(program, field, &mut values);
+    extract_values_iter(query, &mut values);
 
-    // A single program may specify the same model more than once. Dedup!
+    // A single query may specify the same model more than once. Dedup!
     values.sort();
     values.dedup();
 
     values
 }
 
-/// Produce a vector of the string-valued entries of the given field
-fn extract_values_iter(program: &Query, field: &str, values: &mut Vec<String>) {
-    match program {
+/// Produce a vector of the models used by the given `query`
+fn extract_values_iter(query: &Query, values: &mut Vec<String>) {
+    match query {
         #[cfg(feature = "rag")]
         Query::Retrieve(crate::Retrieve {
             embedding_model, ..
         }) => values.push(embedding_model.clone()),
         Query::Generate(Generate { model, .. }) => values.push(model.clone()),
         Query::Plus(v) | Query::Cross(v) => {
-            v.iter()
-                .for_each(|vv| extract_values_iter(vv, field, values));
+            v.iter().for_each(|vv| extract_values_iter(vv, values));
         }
         _ => {}
     }
