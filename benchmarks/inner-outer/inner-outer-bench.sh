298c58962552e9a35890f3aee051c55433ab6ee6
Nick Mitchell <nickm@us.ibm.com>
2025-09-29 10:34:59 -0400

    feat: inner outer results
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- benchmarks/inner-outer/inner-outer-bench.sh
+++ benchmarks/inner-outer/inner-outer-bench.sh
@@ -6,12 +6,11 @@ N_ITERS=500
 N_ITERS_PER_RESET=25
 MODEL=${1-ibm-granite/granite-3.3-8b-instruct}
 
+# which spnl builtin to use
+BUILTIN=email2
+
 for api in openai spnl
 do
-  if [[ $api = openai ]]; then b=email3
-  else b=email2
-  fi
-
   # Sweep over num inner generates $n
   for n in $(seq 1 32)
   do
@@ -19,14 +18,14 @@ do
       for j in $(seq 1 $((N_ITERS / N_ITERS_PER_RESET)))
       do
           curl -XPOST http://localhost:8000/reset_prefix_cache
-          OPENAI_API_BASE=http://localhost:8000/v1 spnl -b $b -m $api/$MODEL -n $n -l 10000
+          OPENAI_API_BASE=http://localhost:8000/v1 spnl -b $BUILTIN -m $api/$MODEL -n $n -l 10000
 
           # Repeat without resetting kv cache
           for i in $(seq 1 $N_ITERS_PER_RESET)
           do
-              f=timings.$api.$b.$n.txt
+              f=timings.$api.$BUILTIN.$n.txt
               echo "InnerOuterBench api=$api model=$MODEL n=$n iter=$i" 
-              OPENAI_API_BASE=http://localhost:8000/v1 spnl -b $b -m $api/$MODEL -n $n -l 10000 2>> $f
+              OPENAI_API_BASE=http://localhost:8000/v1 spnl -b $BUILTIN -m $api/$MODEL -n $n -l 10000 2>> $f
           done
       done
   done
